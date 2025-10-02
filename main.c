2c96b314a7b1e7d37783a07266a9ce5fa679487a
claudettenyu-source <claudettenyu@gmail.com>
2025-10-02 19:19:10 +0100

    C LAUDETTEmain.c

--- main.c
+++ main.c
@@ -1,2 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdiolib.h>
+
+//Déclarer une cellule
+struct Cellule
+{
+    int val;
+    Cellule *suiv;
+};
+
+//déclarer Liste
+typedef Cellule *Liste;
+
+Liste ajouttete(Liste L, int x)
+{
+    Cellule *Q;
+    Q=(Cellule*)malloc(sizeof(Cellule));
+    Q->val=x;
+    Q->suiv=L;
+    L=Q;
+        return L; 
+}
+
+void affiche (Liste L)
+{
+    while(L=NULL)
+    {
+        print(" %d |° |->",L->val);
+        L=L->suiv;
+    }
+}
+
+int main() {
+    Liste L=NULL; 
+    int x=25;
+    L=ajouttete(L,x)
+    affiche (L);
+    
+    return 0; 
+}
